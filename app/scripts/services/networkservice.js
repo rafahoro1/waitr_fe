0325818fcc494ff6d9b89e786dbf73c300571106
rafahoro <gitlab@novil.com.ar>
2017-04-19 02:09:47 -0300

    networkService: implement shared get/post methods

--- app/scripts/services/networkservice.js
+++ app/scripts/services/networkservice.js
@@ -1,13 +1,55 @@
-'use strict';
+(function () {
+  'use strict';
 
-/**
- * @ngdoc service
- * @name waitrFeApp.networkService
- * @description
- * # networkService
- * Service in the waitrFeApp.
- */
-angular.module('waitrFeApp')
-  .service('networkService', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+  /**
+   * @ngdoc service
+   * @name waitrFeApp.networkService
+   * @description
+   * # networkService
+   * Service in the waitrFeApp.
+   */
+  angular.module('waitrFeApp')
+    .service('networkService', ['$q', '$http', networkService]);
+
+  function networkService($q, $http) {
+    var service = {
+      'get': get,
+      'post': post
+    };
+    return service;
+
+    /////////////////////////////////
+
+    function get(uri) {
+      var delay = $q.defer();
+      $http.get(uri).then(
+        function (response) {
+          delay.resolve(response.data);
+        },
+        function (response) {
+          delay.reject(response);
+        });
+      return delay.promise;
+    }
+
+    function post(uri, obj, params) {
+      var delay = $q.defer();
+      if (params) {
+        uri = uri + '?';
+        angular.forEach(params, function (param) {
+          uri = uri + (param.name + '=' + param.value) + '&';
+        });
+        uri = uri.slice(0, -1);
+      }
+      $http.post(uri, obj).then(
+        function (response) {
+          delay.resolve(response.data);
+        },
+        function (response) {
+          delay.reject(response);
+        });
+      return delay.promise;
+    }
+  }
+
+}());
