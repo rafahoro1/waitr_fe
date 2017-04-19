dbdc7b4f08a7d92e37701bf9aeb59172c31853ce
rafahoro <gitlab@novil.com.ar>
2017-04-19 01:03:24 -0300

    Add networkService using yeoman

---
+++ app/scripts/services/networkservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name waitrFeApp.networkService
+ * @description
+ * # networkService
+ * Service in the waitrFeApp.
+ */
+angular.module('waitrFeApp')
+  .service('networkService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
