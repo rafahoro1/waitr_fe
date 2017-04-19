2d8e4bc578d8d0560b337f2a7eae1f218241479b
rafahoro <gitlab@novil.com.ar>
2017-04-19 02:19:41 -0300

    Show (hardcoded) list of drivers on Home page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,37 @@
-'use strict';
+(function () {
+  'use strict';
 
-/**
- * @ngdoc function
- * @name waitrFeApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the waitrFeApp
- */
-angular.module('waitrFeApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  /**
+   * @ngdoc function
+   * @name waitrFeApp.controller:MainCtrl
+   * @description
+   * # MainCtrl
+   * Controller of the waitrFeApp
+   */
+  angular.module('waitrFeApp')
+    .controller('MainCtrl', ['$scope', MainCtrl]);
+
+
+  function MainCtrl($scope) {
+
+    angular.extend($scope, {
+      init: init
+    });
+
+    //////////
+
+    function init(){
+      console.log('in MainCtrl');
+      $scope.drivers = [
+        'dr_1',
+        'dr_2',
+        'dr_3',
+        'dr_4',
+        'dr_5',
+        'dr_6',
+        'dr_nr'
+      ];
+    }
+  };
+
+})();
