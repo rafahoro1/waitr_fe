606c1d887a466e56c0fe5180f8d3300c8a5c090a
rafahoro <gitlab@novil.com.ar>
2017-04-20 02:25:56 -0300

    Add constants - using yeoman generator

---
+++ app/scripts/services/constants.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name waitrFeApp.constants
+ * @description
+ * # constants
+ * Constant in the waitrFeApp.
+ */
+angular.module('waitrFeApp')
+  .constant('constants', 42);
